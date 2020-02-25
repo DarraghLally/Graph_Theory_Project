1ca41a109d304b17529f1514294310fe43fae9d3
Darragh Lally <darragh.lally@gmail.com>
2020-02-25 11:40:02 +0000

    Putting it all together, practicing copy and paste

---
+++ project.py
@@ -0,0 +1,91 @@
+# Fragment class for use in Thompsons Construction
+# Darragh Lally
+
+##############################################################
+
+# Fragment class
+class Fragment:
+    # Start state of NFA frag
+    start = None
+    # Accept state of NFA frag
+    accept = None
+
+    # Constructor
+    def __init__(self, start, accept):
+        self.start = start
+        self.accept = accept
+
+##############################################################
+
+# State Class
+class State:
+    # Every state has n arrows coming from it (0-2)
+    edges = []
+
+    # Label for arrow
+    label = None
+
+    # Constructor
+    def __init__(self, label=None, edges=[]):
+        self.edges = edges
+        self.label = label
+
+myInstance = State(label='a', edges=[])
+myOtherInstance = State(edges=[myInstance])
+
+print(myInstance.label)
+print(myOtherInstance.edges[0])
+
+##############################################################
+
+# Shunting Yard Algorithm
+# input
+infix = "(a|b).c*"
+print("Imput is: ", infix)
+
+# Expected output: ab|c*.
+print("Expect: ", "ab|c*")
+
+# Convert input to a stackish list
+#   put on a list, in reverse order
+infix = list(infix)[::-1]
+
+# Operator Stack
+opers = []
+
+# Output list
+postfix = []
+
+# Operator presidence - add + operator in future
+prec = {'*':100, '.':80, '|':60, ')':40, '(':20}
+
+# Loop through the input one char at a time
+while infix:
+    # Pop a char from the input
+    c = infix.pop() # returns and removes last element
+    if c == '(':
+        # if its an open bracket push to opers stack
+        opers.append(c)
+    elif c == ')':
+        # pop the operators stack until you find an open bracket
+        while opers[-1] != '(':
+            postfix.append(opers.pop())
+        # get rid of the open bracket
+        opers.pop()
+    # Decide what to do based on the char
+    elif c in prec:
+       # push any operators on the opers stack with higher preference to the output
+       while opers and prec[c] < prec[opers[-1]]:
+            postfix.append(opers.push())
+        # Do something here, push c to opers stack
+       opers.append(c)
+    else:
+        # Typically, we just push the char to the output
+        postfix.append(c)
+
+# Pop all operators to the output
+while opers:
+    postfix.append(opers.pop())
+
+# Convert output list to string
+postfix = ''.join(postfix)
