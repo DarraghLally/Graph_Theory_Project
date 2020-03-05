1fa9132a633c8325fca8c5a9dd557e877fa26102
Darragh Lally <darragh.lally@gmail.com>
2020-03-05 10:04:08 +0000

    Working on project.py, getting upto date with video on Automaton fragments

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-# Fragment class for use in Thompsons Construction
+# Project implementation
 # Darragh Lally
 
 ##############################################################
@@ -30,62 +30,156 @@ class State:
         self.edges = edges
         self.label = label
 
-myInstance = State(label='a', edges=[])
-myOtherInstance = State(edges=[myInstance])
+# Testing block
 
-print(myInstance.label)
-print(myOtherInstance.edges[0])
+# myInstance = State(label='a', edges=[])
+# myOtherInstance = State(edges=[myInstance])
+# print(myInstance.label)
+# print(myOtherInstance.edges[0])
+
+#End testing block
 
 ##############################################################
 
 # Shunting Yard Algorithm
-# input
-infix = "(a|b).c*"
-print("Imput is: ", infix)
+def shunt(infix):
 
-# Expected output: ab|c*.
-print("Expect: ", "ab|c*")
+# Below block used for testing...
+    
+    # Convert 'infix' to a stack list
+    # infix = "(a|b).c*"
+    # print("Input: (a|b).c*")
+    # Expected output: ab|c*.
+    # print("Expect: ", "ab|c*")
 
-# Convert input to a stackish list
-#   put on a list, in reverse order
-infix = list(infix)[::-1]
+# End testing block
 
-# Operator Stack
-opers = []
+    # Convert input to a stackish list put on a list, in reverse order
+    infix = list(infix)[::-1]
 
-# Output list
-postfix = []
+    # Operator Stack
+    opers = []
 
-# Operator presidence - add + operator in future
-prec = {'*':100, '.':80, '|':60, ')':40, '(':20}
+    # Output list
+    postfix = []
 
-# Loop through the input one char at a time
-while infix:
-    # Pop a char from the input
-    c = infix.pop() # returns and removes last element
-    if c == '(':
-        # if its an open bracket push to opers stack
-        opers.append(c)
-    elif c == ')':
-        # pop the operators stack until you find an open bracket
-        while opers[-1] != '(':
-            postfix.append(opers.pop())
-        # get rid of the open bracket
-        opers.pop()
-    # Decide what to do based on the char
-    elif c in prec:
-       # push any operators on the opers stack with higher preference to the output
-       while opers and prec[c] < prec[opers[-1]]:
-            postfix.append(opers.push())
-        # Do something here, push c to opers stack
-       opers.append(c)
-    else:
-        # Typically, we just push the char to the output
-        postfix.append(c)
+    # Operator presidence - add + operator in future
+    prec = {'*':100, '.':80, '|':60, ')':40, '(':20}
 
-# Pop all operators to the output
-while opers:
-    postfix.append(opers.pop())
+    # Loop through the input one char at a time
+    while infix:
+        # Pop a char from the input
+        c = infix.pop() # Returns and removes last element
+        if c == '(':
+            # If its an open bracket push to opers stack
+            opers.append(c)
+        elif c == ')':
+            # Pop the operators stack until you find an open bracket
+            while opers[-1] != '(':
+                postfix.append(opers.pop())
+            # Get rid of the open bracket
+            opers.pop()
+        # Decide what to do based on the char
+        elif c in prec:
+            # Push any operators on the opers stack with higher preference to the output
+            while opers and prec[c] < prec[opers[-1]]:
+                postfix.append(opers.pop())
+            # Push c to opers stack
+            opers.append(c)
+        else:
+            # Typically, we just push the char to the output
+            postfix.append(c)
 
-# Convert output list to string
-postfix = ''.join(postfix)
+    # Pop all operators to the output
+    while opers:
+        postfix.append(opers.pop())
+
+    # Convert output list to string
+    postfix = ''.join(postfix)
+
+##############################################################
+
+def regex_compile(infix):
+    # First convert infix to postfix
+    postfix = shunt(infix)
+    # Parse postfix string into a list and reverse
+    postfix = list(postfix)[::-1]
+    
+    # NFA stact
+    nfa_stack = []
+    
+    while(postfix):
+        # Pop a char from postfix
+        c = postfix.pop()
+        if c == '.':
+            # Pop two fragments from NFA stack
+            frag1 = nfa_stack.pop()
+            frag2 = nfa_stack.pop()
+            # Point frag2's accept state at frag1's start state
+            frag2.accept.edges.append(frag1.start)
+            # Create new fragment using frag2's start state and frag1's accept state
+            newfrag = Fragment(frag2.start, frag1.accept)
+        elif c == '|':
+            # Pop two fragments from NFA stack
+            frag1 = nfa_stack.pop()
+            frag2 = nfa_stack.pop()
+            # Create new start and accept states
+            start = State()
+            accept = State(edges=[frag2.start, frag1.accept])
+            # Point the old accept states to the new one
+            frag2.edges.append(accept)
+            frag1.edges.append(accept)
+            # Create a new fragment
+            newfrag = Fragment(start, accept)
+        #elif c == '+':
+            #
+        elif c == '*':
+            # Pop just one fragment, Kleene star is a binary operator!
+            frag = nfa_stack.pop()
+            # Create new start and accept states
+            start = State()
+            accept = State(edges=[accept, frag.start])
+            # Point to old accept states
+            frag.accept.edges = [frag.start,accept]
+            # Create new fragment
+            newfrag = Fragment(start, accept)
+        else:
+            start = State(label=c, edges=[accept])
+            accept = State()
+            #create a new fragment with initial and accept
+            newfrag = Fragment(start, accept)
+        
+        # Push new fragment to the NFA stack
+        nfa_stact.append(newfrag)
+
+    # NFA stack should have exactly one NFA
+    return nfa_stack.pop()
+
+##############################################################
+
+# Function to return true if the regular expression 'regex'
+# matches the string 's'
+def match(regex, s):
+    # Compile regex into NFA
+    nfa = regex_compile(regex)
+    return nfa
+
+##############################################################
+
+# Testing match()
+print(match("a.b|b*", "ab"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
